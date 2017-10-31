1ac267287652700bd7e4a9ed513a79091aa1ace6
Roberto Halgravez <roberto.halgravez@unosquare.com>
2017-10-31 11:22:43 -0600

    MOB-391 Add file to handle the keys for the standard flow as constants

---
+++ Samples/SimpleCaptureDemo/JRCapture/JRStandardFlowKeys.h
@@ -0,0 +1,17 @@
+//
+//  JRStandardFlowKeys.h
+//  SimpleCaptureDemo
+//
+//  Created by Roberto Halgravez on 10/31/17.
+//
+//
+
+extern NSString *const kJRCaptureFlowKey;
+extern NSString *const kFieldsKey;
+extern NSString *const kLabelKey;
+extern NSString *const kPlaceholderKey;
+extern NSString *const kSchemeIdKey;
+extern NSString *const kOptionsKey;
+extern NSString *const kDisabledKey;
+extern NSString *const kTextKey;
+extern NSString *const kValueKey;
