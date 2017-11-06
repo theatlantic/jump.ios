8c9c386fbc39a84dbb7436d90f9b0b6d2153e6e3
Roberto Halgravez <roberto.halgravez@unosquare.com>
2017-11-06 14:09:25 -0600

    MOB-391 Add the code so the user can scroll all the view, also add the textField Delegate so the textField doesn't get overlap by the keyboard

--- Samples/SimpleCaptureDemo/SimpleCaptureDemo/Classes/CaptureDynamicForm.m
+++ Samples/SimpleCaptureDemo/SimpleCaptureDemo/Classes/CaptureDynamicForm.m
@@ -8,9 +8,11 @@
 
 static NSMutableDictionary *identifierMap = nil;
 
-@interface CaptureDynamicForm () <JRCaptureDelegate>
+@interface CaptureDynamicForm () <UITextFieldDelegate ,JRCaptureDelegate>
 
 @property(strong, nonatomic) JRCaptureUser *captureUser;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *middleNameTextField;
@@ -39,12 +41,40 @@ static NSMutableDictionary *identifierMap = nil;
 @end
 
 @implementation CaptureDynamicForm
+{
+    UIView *activeField;
+}
 
 #pragma mark - Lifecycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.scrollView.contentSize = CGSizeMake(320, self.optInRegistrationSwitch.frame.origin.y + (self.optInRegistrationSwitch.frame.size.height) + 40);
+    
+    if ([self respondsToSelector:@selector(setEdgesForExtendedLayout:)]) {
+        [self setEdgesForExtendedLayout:UIRectEdgeNone];
+    }
+    
     self.captureUser = [JRCaptureUser captureUser];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+    self.firstNameTextField.delegate         = self;
+    self.middleNameTextField.delegate        = self;
+    self.lastNameTextField.delegate          = self;
+    self.emailTextField.delegate             = self;
+    self.displayNameTextField.delegate       = self;
+    self.passwordTextField.delegate          = self;
+    self.mobileTextField.delegate            = self;
+    self.phoneTextField.delegate             = self;
+    self.address1TextField.delegate          = self;
+    self.address2TextField.delegate          = self;
+    self.addressCityTextField.delegate       = self;
+    self.addressPostalCodeTextField.delegate = self;
 }
 
 #pragma mark - Action
@@ -69,6 +99,32 @@ static NSMutableDictionary *identifierMap = nil;
     
     [JRCapture registerNewUser:self.captureUser socialRegistrationToken:nil forDelegate:self];
     self.registerButton.enabled = NO;
+}
+
+#pragma mark - Notifications
+- (void)keyboardDidShow:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0);
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+    
+    CGRect frame = self.view.frame;
+    frame.size.height -= keyboardSize.height;
+    CGPoint origin = activeField.frame.origin;
+    CGPoint bottom = CGPointMake(origin.x, origin.y + activeField.frame.size.height);
+    if (!CGRectContainsPoint(frame, origin) || !CGRectContainsPoint(frame, bottom )) {
+        [self.scrollView scrollRectToVisible:activeField.frame animated:YES];
+    }
+}
+
+- (void)keyboardWillHide:(NSNotification*)aNotification
+{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
 }
 
 #pragma mark - JRCaptureDelegate
@@ -96,6 +152,26 @@ static NSMutableDictionary *identifierMap = nil;
     self.registerButton.enabled = YES;
 }
 
+#pragma mark - UITextFieldDelegate
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    activeField = textField;
+}
 
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    if (textField == activeField) {
+        activeField = nil;
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if (textField == activeField) {
+        activeField = nil;
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
 
 @end
