0dd02f691826e27e6a77ae163bb10d266cdac509
pbjanrain <paulbaker@janrain.com>
2018-03-09 10:06:43 -0800

    General clean up

--- Samples/SimpleCaptureDemoNative/SimpleCaptureDemoNative/Classes/UIAlertController+JRAlertController.h
+++ Samples/SimpleCaptureDemoNative/SimpleCaptureDemoNative/Classes/UIAlertController+JRAlertController.h
@@ -29,12 +29,10 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
-extern NSString *const kJRCaptureFlowKey;
-extern NSString *const kFieldsKey;
-extern NSString *const kLabelKey;
-extern NSString *const kPlaceholderKey;
-extern NSString *const kSchemeIdKey;
-extern NSString *const kOptionsKey;
-extern NSString *const kDisabledKey;
-extern NSString *const kTextKey;
-extern NSString *const kValueKey;
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (JRAlertController)
+
++(id)alertControllerWithTitle:(NSString *)title message:(NSString *)message alertActions:(UIAlertAction *)alertActions, ...;
+
+@end
